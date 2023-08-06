3d1603811c7bb8df6208f234dddcbe6ddfed9156
DariiaKorovchenko <samurais.daughter007@gmail.com>
2023-08-06 13:06:25 +0300

    created phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,82 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import { nanoid } from 'nanoid';
+import { Form } from './Form/Form';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+
+export class App extends React.Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  getContactListData = data => {
+    this.setState(prevState => {
+      if (prevState.contacts.length === 0) {
+        return {
+          contacts: [{ name: data.name, number: data.number, id: nanoid() }],
+        };
+      } else {
+        const including = prevState.contacts.some(contact =>
+          contact.name.toLowerCase().includes(data.name.toLowerCase())
+        );
+        if (including) {
+          alert(`${data.namme} is already in contacts.`);
+          return {
+            contacts: [...prevState.contacts],
+          };
+        } else {
+          return {
+            contacts: [
+              ...prevState.contacts,
+              { name: data.name, number: data.number, id: nanoid() },
+            ],
+          };
+        }
+      }
+    });
+  };
+
+  changeFilter = event => {
+    this.setState({ filter: event.currentTarget.value });
+  };
+
+  visibleContacts() {
+    const visibleContacts = this.state.contacts.filter(contact => {
+      return contact.name
+        .toLowerCase()
+        .includes(this.state.filter.toLowerCase());
+    });
+    return visibleContacts;
+  }
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <Form onSubmit={this.getContactListData} />
+
+        <h2>Contacts</h2>
+        <Filter value={this.state.filter} filterData={this.changeFilter} />
+
+        {this.state.filter === null ? (
+          <ContactList
+            contacts={this.state.contacts}
+            delete={this.deleteContact}
+          />
+        ) : (
+          <ContactList
+            contacts={this.visibleContacts()}
+            delete={this.deleteContact}
+          />
+        )}
+      </div>
+    );
+  }
+}
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from './ContactList.module.css';
+
+export const ContactList = ({ contacts, deleteContact }) => {
+  return (
+    <ul>
+      {contacts.map(({ name, number, id }) => {
+        return (
+          <li key={id} className={css.contact_item}>
+            {name}: {number}
+            <button
+              type="button"
+              className={css.delete_btn}
+              onClick={() => deleteContact(id)}
+            >
+              Delete
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+ContactList.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  delete: PropTypes.func.isRequired,
+};
